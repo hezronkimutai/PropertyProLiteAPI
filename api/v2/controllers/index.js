e81d6f8c5678f0ff9d6f5167fe45c88e500b8a6a
hezronkimutai <hezronchelimo.hc@gmail.com>
2019-06-23 18:37:54 +0300

    Finished #166860993 Incorperated JWT based authentication

--- api/v2/controllers/index.js
+++
@@ -1,9 +0,0 @@
-const routes = require('express').Router();
-
-const properties = require('./properties');
-const users = require('./users')
-
-routes.use('/properties', properties);
-routes.use('/users', users);
-
-module.exports = routes;
