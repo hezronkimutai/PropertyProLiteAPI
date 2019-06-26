29ad4014322f0a66d603bdcc4cd87e3317649c64
hezronkimutai <hezronchelimo.hc@gmail.com>
2019-06-27 00:17:33 +0300

    update

--- test/index.js
+++ test/index.js
@@ -5,15 +5,9 @@ const chaiHttp = require('chai-http');
 
 const should = chai.should();
 chai.use(chaiHttp);
-<<<<<<< HEAD
-
-const users = require('./users')
-const properties = require('./properties')
-=======
 
 const usersv1 = require('./v1/users')
 const propertiesv1 = require('./v1/properties')
 
 // const usersv2 = require('./v2/users')
 // const propertiesv2 = require('./v2/properties')
->>>>>>> starts #166932722 created  property tests
